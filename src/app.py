bd30b9048ad880011fd46a7315afe4e8415c8a42
aumthakkar <aumthakkar@yahoo.co.uk>
2025-07-29 00:06:52 +0100

    first commit of the app

---
+++ src/app.py
@@ -0,0 +1,26 @@
+
+from flask import Flask, jsonify
+import time
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+
+def info():
+    return jsonify({
+      'time' : time.ctime(),
+      'hostname' : socket.gethostname()  
+    })
+
+@app.route('/api/v1/healthz')
+
+def healthz():
+    return jsonify({
+      'status' : 'up'
+    }), 200
+
+# main driver function
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0")
